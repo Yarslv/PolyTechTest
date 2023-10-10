bae36a3bbbe9189840a9c637ced15fa0ce3b3f74
Yapslv <yaroslav.prodan@onix-system.com>
2023-10-10 10:15:19 +0300

    Initial commit

--- app/src/main/java/com/polytechtest/util/Constants.kt
+++ app/src/main/java/com/polytechtest/util/Constants.kt
@@ -3,6 +3,6 @@ package com.polytechtest.util
 object Constants {
     const val BASE_URL = "https://api.nytimes.com/svc/books/v3/"
     const val API_KEY_NAME = "api-key"
-    const val API_KEY_VALUE = ""
+    const val API_KEY_VALUE = "jC20ao9GDfdQXNUsKRB5svWHZASzKVor"
     const val DATABASE_NAME = "database-name"
 }
